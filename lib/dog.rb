3404e8ed37461715fb122071b5467d090475f41f
Ryan Schmelter <rschmelter33@gmail.com>
2018-02-10 17:15:35 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -33,7 +33,7 @@ class Dog
 
   def update
     sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
-    DB[:conn]execute(sql, self.name, self.breed, self.id)
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
   def self.find_by_name(name)
