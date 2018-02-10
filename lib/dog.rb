a4a63f379de72b1f4ea799e9c1837329a52f5cc0
Ryan Schmelter <rschmelter33@gmail.com>
2018-02-10 17:21:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -50,10 +50,10 @@ class Dog
   end
 
   def save
-    if self.id 
+    if self.id
       self.update
-    else 
-      sql = <<-SQL 
+    else
+      sql = <<-SQL
         INSERT INTO dogs (name, breed)
         VALUES (?, ?)
       SQL
